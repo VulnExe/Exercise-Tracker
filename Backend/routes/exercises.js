d46b9c345a76f331b76da4706422628636c4f292
Jerry <jerryjvj33@gmail.com>
2022-05-30 22:59:46 +0530

    completed backend logics

--- Backend/routes/exercises.js
+++ Backend/routes/exercises.js
@@ -26,5 +26,34 @@ const Exercise = require('../models/exercise.model')
  })
 
 
+ router.get("/:id",(req,res)=>{
+     Exercise.findById(req.params.id)
+     .then((exercise)=>res.json(exercise))
+     .catch((err)=>res.status(400).json("Error :" + err))
+ })
+
+router.delete("/:id",(req,res)=>{
+    Exercise.findByIdAndDelete(req.params.id)
+    .then((exercise)=>res.json(exercise))
+    .catch((err)=>res.status(400).json("Error :"+ err))
+})
+
+router.post("/update/:id",(req,res)=>{
+    Exercise.findByIdAndUpdate(req.params.id)
+    .then((exercise)=>{
+        exercise.username = req.body.username;
+        exercise.description = req.body.description;
+        exercise.duration = Number(req.body.duration);
+        exercise.date = Date.parse(req.body.date);
+
+        exercise.save()
+        .then(()=> res.json("updated successfully!!!"))
+        .catch((err)=>res.status(400).json("Error :" + err))
+
+    })
+    .catch((err)=>res.status(400).json("error :" + err))
+})
+
+
 
 module.exports = router 
